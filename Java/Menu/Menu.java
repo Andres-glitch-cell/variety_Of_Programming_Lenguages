a371b87ed3c9270542d24fb0fe4cf5564226914e
Andrés Fernández <afernandezs.iesjc@gmail.com>
2025-03-16 17:14:55 +0100

    Menu-Case-1-2-3

--- Java/Menu/Menu.java
+++ Java/Menu/Menu.java
@@ -9,7 +9,7 @@ public class Menu {
 
         Scanner sc = new Scanner(System.in);
         int numeroProductos = 0;
-        Producto[] numeroProductosIntroducidos = null;
+        Producto[] numeroProductosIntroducidosUsuario = null;
         int contadorProductos = 0;
         int opcion;
         String[] numeroDeProductosUsuario = null;
@@ -35,7 +35,7 @@ public class Menu {
                 case 1:
                     System.out.println("Introduce el número de productos que quieres añadir:");
                     numeroProductos = sc.nextInt();
-                    numeroProductosIntroducidos = new Producto[numeroProductos];
+                    numeroProductosIntroducidosUsuario = new Producto[numeroProductos];
                     System.out.println("Se ha creado espacio para " + numeroProductos + " productos");
                     if (numeroProductos <= 0) {
                         System.out.println("No puedes continuar si no introduces una cantidad de productos (Mínimo 1)");
@@ -44,7 +44,7 @@ public class Menu {
 
                 // 2. Dar de alta un producto
                 case 2:
-                    if (numeroProductosIntroducidos == null) {
+                    if (numeroProductosIntroducidosUsuario == null) {
                         System.out.println("Tienes que introducir al menos 1 producto antes...");
                     } else {
                         String[] nombreProducto = new String[numeroProductos]; // Inicializar array correctamente
@@ -53,19 +53,19 @@ public class Menu {
                         sc.nextLine(); // Limpiar buffer
 
                         do {
-                            for (int i = 0; i < numeroProductosIntroducidos.length; i++) {
+                            for (int i = 0; i < numeroProductosIntroducidosUsuario.length; i++) {
                                 System.out.println("Introduce el nombre del producto " + (i + 1) + ":");
                                 nombreProducto[i] = sc.nextLine();
                                 numeroDeProductosUsuario[i] = nombreProducto[i];
                             }
 
                             System.out.println("Los nombres de los productos son:");
-                            for (int i = 0; i < numeroProductosIntroducidos.length; i++) {
+                            for (int i = 0; i < numeroProductosIntroducidosUsuario.length; i++) {
                                 System.out.println("Producto " + (i + 1) + ": " + nombreProducto[i]);
                             }
 
                             // Para cada producto, pedir su precio
-                            for (int i = 0; i < numeroProductosIntroducidos.length; i++) {
+                            for (int i = 0; i < numeroProductosIntroducidosUsuario.length; i++) {
                                 System.out.println("\nPara el producto " + nombreProducto[i]);
                                 System.out.println("\n¿Quieres introducir un precio con decimales? (S/N)");
                                 char SioNo = sc.next().charAt(0);
@@ -74,45 +74,55 @@ public class Menu {
                                     System.out.println("Introduce el precio con decimales:");
                                     precioProductoConDecimales = sc.nextDouble();
                                     // Crear y guardar el producto
-                                    numeroProductosIntroducidos[i] = new Producto(nombreProducto[i],
+                                    numeroProductosIntroducidosUsuario[i] = new Producto(nombreProducto[i],
                                             precioProductoConDecimales, 0);
                                 } else if (SioNo == 'N') {
                                     System.out.println("Introduce el precio sin decimales:");
                                     precioProductoSinDecimales = sc.nextInt();
                                     // Crear y guardar el producto
-                                    numeroProductosIntroducidos[i] = new Producto(nombreProducto[i], 0,
+                                    numeroProductosIntroducidosUsuario[i] = new Producto(nombreProducto[i], 0,
                                             precioProductoSinDecimales);
                                 }
                                 contadorProductos++;
 
-                                // Esta parte lo admito lo ha hecho la IA por que ni yo me aclaría
+                                // Esta parte lo admito lo ha hecho la IA por que ni yo me aclaría (Los
+                                // comentarios si que son mios)
                                 System.out.println("Los precios con los nombres de los productos son:");
-                                for (int y = 0; y < numeroProductosIntroducidos.length; y++) {
-                                    if (numeroProductosIntroducidos[y].getPrecioConDecimales() > 0) {
+                                // Bucle que recorre la longitud de la array - 1
+                                for (int y = 0; y < numeroProductosIntroducidosUsuario.length; y++) {
+                                    /*
+                                     * (Primero se pone la CANTIDAD DE PRODUCTOS DEL USUARIO)
+                                     * Y a continuación, si el precio con decimales es mayor que 0,
+                                     * mostrar el precio con decimales
+                                     */
+                                    if (numeroProductosIntroducidosUsuario[y].getPrecioConDecimales() > 0) {
                                         System.out.println("Producto " + (y + 1) + ": " + nombreProducto[y] +
-                                                " - Precio: " + numeroProductosIntroducidos[y].getPrecioConDecimales()
+                                                " - Precio: "
+                                                + numeroProductosIntroducidosUsuario[y].getPrecioConDecimales()
                                                 + "€");
                                     } else {
                                         System.out.println("Producto " + (y + 1) + ": " + nombreProducto[y] +
-                                                " - Precio: " + numeroProductosIntroducidos[y].getPrecioSinDecimales()
+                                                " - Precio: "
+                                                + numeroProductosIntroducidosUsuario[y].getPrecioSinDecimales()
                                                 + "€");
                                     }
                                 }
                             }
-                            break; // Salir del do-while después de procesar todos los productos
-                        } while (false); // Cambiado para que solo se ejecute una vez
+                            break;
+                        } while (nombreProducto == null || precioProductoSinDecimales <= 0);
                     }
                     break;
 
                 // 3. Visualizar los productos
                 case 3:
-                    if (numeroProductosIntroducidos == null || contadorProductos == 0) {
+                    if (numeroProductosIntroducidosUsuario == null || contadorProductos == 0) {
                         System.out.println("No has introducido ningun producto para mostrar");
                     } else {
                         for (int i = 0; i < contadorProductos; i++) {
-                            System.out.println("Producto " + (i + 1) + ": " + numeroProductosIntroducidos[i].getNombre()
-                                    + " " + numeroProductosIntroducidos[i].getPrecioConDecimales() + " " +
-                                    numeroProductosIntroducidos[i].getPrecioSinDecimales());
+                            System.out.println("Producto " + (i + 1) + ": "
+                                    + numeroProductosIntroducidosUsuario[i].getNombre()
+                                    + " " + numeroProductosIntroducidosUsuario[i].getPrecioConDecimales() + " " +
+                                    numeroProductosIntroducidosUsuario[i].getPrecioSinDecimales());
                         }
                     }
                     break;
