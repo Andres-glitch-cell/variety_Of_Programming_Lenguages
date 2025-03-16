4a7fb8c7d63d6d478a63d1f244c8d8dcf5ae4eff
Andrés Fernández <afernandezs.iesjc@gmail.com>
2025-03-16 16:03:02 +0100

    MenuModificado

--- Java/Menu/Menu.java
+++ Java/Menu/Menu.java
@@ -7,6 +7,8 @@ public class Menu {
 
         Scanner sc = new Scanner(System.in);
 
+        int numeroProductos = 0;
+
         System.out.println("--Menu--");
         System.out.println("1. Introduce cuantos productos quieres añadir al código");
         System.out.println("2. Dar de alta un producto");
@@ -18,44 +20,50 @@ public class Menu {
         System.out.println("8. Salir");
         System.out.println("Elige una opción:");
         int opcion = sc.nextInt();
-        sc.close();
 
         switch (opcion) {
             case 1:
                 // 1º Introduce cuantos productos quieres añadir al código
                 System.out.println("Introduce el número de productos que quieres añadir:");
-                int numeroProductos = sc.nextInt();
+                numeroProductos = sc.nextInt();
                 Producto[] numeroProductosIntroducidos = new Producto[numeroProductos];
                 System.out.println("Se ha creado espacio para " + numeroProductos + " productos");
+                if (numeroProductos >= numeroProductosIntroducidos.length) {
+                    
+                }
                 break;
 
             // 2º Dar de alta un producto
             case 2:
-                String nombreProducto;
-                double precioProductoConDecimales = 0;
-                int precioProductoSinDecimales = 0;
-                do {
-                    System.out.println("Introduce el nombre del producto:");
-                    nombreProducto = sc.next();
-                    System.out.println("Quieres introducir un precio del producto que sea con decimales? (S / N)");
-                    char SioNo = sc.next().charAt(0);
+                if (numeroProductos <= 0) {
+                    System.out.println("Introduce un número valido antes de continuar...");
+                } else {
+                    String nombreProducto;
+                    double precioProductoConDecimales = 0;
+                    int precioProductoSinDecimales = 0;
+                    do {
+                        System.out.println("Introduce el nombre del producto:");
+                        nombreProducto = sc.next();
+                        System.out.println("Quieres introducir un precio del producto que sea con decimales? (S / N)");
+                        char SioNo = sc.next().charAt(0);
 
-                    if (SioNo == 'S') {
-                        System.out.println("Introduce el precio del producto (con decimales):");
-                        precioProductoConDecimales = sc.nextDouble();
-                    } else if (SioNo == 'N') {
-                        System.out.println("Introduce el precio del producto (sin deciamles):");
-                        precioProductoSinDecimales = sc.nextInt();
-                    } else {
-                        System.out.println("Opción no válida");
-                    }
-                } while (nombreProducto == null && precioProductoSinDecimales == 0.0);
+                        if (SioNo == 'S') {
+                            System.out.println("Introduce el precio del producto (con decimales):");
+                            precioProductoConDecimales = sc.nextDouble();
+                        } else if (SioNo == 'N') {
+                            System.out.println("Introduce el precio del producto (sin deciamles):");
+                            precioProductoSinDecimales = sc.nextInt();
+                        } else {
+                            System.out.println("Opción no válida");
+                        }
+                    } while (nombreProducto == null && precioProductoSinDecimales <= 0.0);
 
-                Producto producto = new Producto(nombreProducto, precioProductoConDecimales,
-                        precioProductoSinDecimales);
-                System.out.println("Producto creado: " + producto.getNombre() + " " + producto.getPrecioConDecimales()
-                        + " " + producto.getPrecioSinDecimales());
+                    Producto producto = new Producto(nombreProducto, precioProductoConDecimales,
+                            precioProductoSinDecimales);
+                    System.out.println("Producto creado: " + producto.getNombre() + " " + producto.getPrecioConDecimales()+ " " + producto.getPrecioSinDecimales());
+                }
                 break;
         }
+        sc.close();
     }
 }
